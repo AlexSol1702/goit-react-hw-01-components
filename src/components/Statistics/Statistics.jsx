54220594b707f8d89e3a2dfc1a1897533e0b989b
AlexSol1702 <105703672+AlexSol1702@users.noreply.github.com>
2022-12-11 19:39:29 +0200

    css

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,6 +3,9 @@ import css from "./Statistics.module.css";
 
 
 export  function Statistics({title, stats}) { 
+  const colorItem =()=> {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ }
     return (
         <div className="container">
           <section className={css.statistics}>
@@ -11,7 +14,7 @@ export  function Statistics({title, stats}) {
           <ul className={css.list}>
              {stats.map
               (({id, label, percentage}) => 
-                  <li className={css.item} key={id}>
+                  <li className={css.item} style={{backgroundColor: colorItem()}} key={id}>
                   <span className={css.label}>{label}</span>    
                   <span className={css.percentage}>{percentage}%</span>
                 </li>                 
